6667772e93d566622155ac0c5d78afdc7f30eded
DavidPPizarro <paulpizarro151@gmail.com>
2025-04-24 21:38:17 -0500

    feat: add PostHeader component and BlogPostLayout for blog posts
    - Added BlogPostLayout to structure blog posts with a main layout and post content area.
    - Implemented slugify and formatDate utility functions for URL slugs and date formatting.

--- src/js/utils.js
+++ src/js/utils.js
@@ -2,9 +2,15 @@ export function slugify(str) {
   return str
     .toLowerCase()
     .toLowerCase()
-    .replcae(/\s+/g, "-")
+    .replace(/\s+/g, "-")
     .replace(/[^\w-]+/g, "")
     .replace(/--+/g, "-")
     .replace(/^-+/, "")
     .replace(/-+$/, "");
+}
+
+export function formatDate(date){
+  return new Date(date).toLocaleDateString('es-PE', {
+    timeZone: 'UTC'
+  });
 }
